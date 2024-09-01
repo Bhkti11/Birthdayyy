e0b740a031d9bcecdb8139cec1d883ecbd5d13c7
Bhkti11 <106379373+Bhkti11@users.noreply.github.com>
2024-09-01 17:15:59 +0530

    Script.js

--- Script.js
+++ Script.js
@@ -1,4 +1,11 @@
-function showMessage() {
+document.getElementById('surpriseButton').addEventListener('click', function() {
     const message = document.getElementById('message');
+    const sound = document.getElementById('birthdaySound');
+
     message.classList.remove('hidden');
-}
+    sound.play();
+
+    this.style.display = 'none';
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?celebration,fireworks')";
+    document.body.style.backgroundSize = 'cover';
+});
